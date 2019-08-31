2b04a39e874a0ca98a2a285f84f9dc8235a296b5
Andrew Williams <1williamsaj@gmail.com>
2019-08-31 19:59:31 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   new_array =[] #We create a new array for our squared values later on.
 
     array.each do |square| #We then run an 'each' statement to iterate each element of the array entered into the method.
-        new_array << square**2
+        new_array << square**2 
       end
 
   new_array
