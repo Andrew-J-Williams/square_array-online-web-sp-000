43a0fa75660b2dea5eddad213fe706e9d6c5c964
Andrew Williams <1williamsaj@gmail.com>
2019-08-31 19:58:18 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,10 @@
 def square_array(array)
 
-new_array =[] #We create a new array for our squared values later on.
+  new_array =[] #We create a new array for our squared values later on.
 
-  array.each do |square| #
-      new_array << square**2
-  end
+    array.each do |square| #
+        new_array << square**2
+      end
 
   new_array
 
