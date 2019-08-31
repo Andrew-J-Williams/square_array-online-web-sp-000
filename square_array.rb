8111f50879bdca12f4f1188e3bee5a48c08be5c0
Andrew Williams <1williamsaj@gmail.com>
2019-08-31 19:56:10 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,14 +1,11 @@
 def square_array(array)
-  new_array =[]
-  count = 0
+
 
   array.each do |square|
-    new_var = square[count]
-    new_val = new_var**2
-    new_array << new_val
-    count+=1
+    
+
   end
 
-  new_array
+
 
 end
